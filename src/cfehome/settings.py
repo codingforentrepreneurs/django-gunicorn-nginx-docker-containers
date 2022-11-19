73c529fa641220ca99216222e4ac774a242c6e54
Justin Mitchel <j@teamcfe.com>
2022-11-18 22:19:56 -0600

    Removed SECRET_KEY from repo

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ccys_8g$-%ws1k7+76w&_y^80ltg*+!coq5p!(ij+e4ho7%$-#"
+# Create a one-off secret key: https://cfe.sh/blog/create-a-one-off-django-secret-key/
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.desalsa.io']
+ALLOWED_HOSTS = ['.codingforentrepreneurs.com']
 
 
 # Application definition
